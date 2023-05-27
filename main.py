e6db0c154f727eeecad3b1bf1623a4aee4259883
Hugo <55044990+HugLep@users.noreply.github.com>
2023-05-27 21:51:07 +0200

    Optimization update

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ primeNumbers = [2]
 otherNumbers = []
 startingNumber = 3
 currentNumber = startingNumber
-divider = 2
+divider = primeNumbers[0]
+x = -1
 
 print("WARNING: Asking for more than 5000 numbers is not recommended, it will require a lot of resources from your computer")
 howManyPrimes = int(input("How many Prime Numbers do you want to have ? "))
@@ -14,20 +15,23 @@ while len(primeNumbers) != howManyPrimes:
     if float(resultIsInt).is_integer():
         otherNumbers.append(currentNumber)
         currentNumber = currentNumber + 1
-        divider = 2
+        divider = primeNumbers[0]
+        x = -1
 
     else:
-        x = divider + 1
-        if x < (currentNumber / 2):
-            divider = x
+        x = x + 1
+        if x < (len(primeNumbers) - 1):
+
+          divider = primeNumbers[x]
         else:
             primeNumbers.append(currentNumber)
             currentNumber = currentNumber + 1
-            divider = 2
+            divider = primeNumbers[0]
+            x = -1
+
 
 print(" ")
 print("Here are ", howManyPrimes, " Prime Numbers that have been generated :")
 print(primeNumbers)
-
 
 
